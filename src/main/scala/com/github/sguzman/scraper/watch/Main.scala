95340820625a69632e34ed300fd0cc6414e9627b
Salvador Guzman <guzmansalv@gmail.com>
2018-03-29 02:30:27 -0700

    working iframe link extract

--- src/main/scala/com/github/sguzman/scraper/watch/Main.scala
+++ src/main/scala/com/github/sguzman/scraper/watch/Main.scala
@@ -1,12 +1,15 @@
 package com.github.sguzman.scraper.watch
 
+import java.util.Base64
+
 import io.circe.generic.auto._
 import io.circe.parser.decode
 import io.circe.syntax._
-import net.ruippeixotog.scalascraper.browser.Browser
+import net.ruippeixotog.scalascraper.browser.{Browser, JsoupBrowser}
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract.elementList
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.scraper.ContentExtractors.element
+import org.apache.commons.lang3.StringUtils
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -46,5 +49,34 @@ object Main {
     }
 
     Init.removeHttp(pages.toList.map(_.link))
+
+    val iframe = episodes
+      .flatMap(_.eps)
+      .map{a =>
+      def proc(doc: Browser#DocumentType): String = {
+        val script = doc.>>(element("""meta[itemprop="embedURL"] + script""")).innerHtml
+        val array = script.split(";")
+
+        val sub = array(2).split("\\)").last.trim.stripPrefix("-").trim.toLong
+
+        val data = StringUtils.substringBetween(array(1), "[", "]")
+        val dataArray = data.replaceAll("\"", "").split(", ").toList
+        val done = dataArray
+          .map(Base64.getDecoder.decode)
+          .map(_.map(_.toChar).mkString)
+          .map(_.replaceAll("[^0-9]", ""))
+          .map(_.toInt)
+          .map(_ - sub)
+          .map(_.toChar)
+          .mkString
+
+        JsoupBrowser().parseString(done).>>(element("iframe[src]")).attr("src")
+      }
+
+      def dec(s: String): String = s
+      Init.cascade(a, proc, dec)
+    }
+
+    Init.removeHttp(episodes.toList.flatMap(_.eps))
   }
 }
